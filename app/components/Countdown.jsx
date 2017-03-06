b67129cda86d64adb8d79bd217a5f3ba178604e3
Antonio Touriño <atourino@gmail.com>
2017-03-05 22:26:17 -0500

    Basic timer and countdown components.

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = (props) => {
+  return (
+    <div>
+      <h2>Countdown!</h2>
+    </div>
+  );
+}
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Timer = (props) => {
+  return (
+    <div>
+      <h2>Timer!</h2>
+    </div>
+  );
+}
+
+module.exports = Timer;
